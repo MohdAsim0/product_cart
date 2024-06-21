9aaf9c45933462f20311d68a6e154b3c0ba6a956
MohdAsim0 <asim75077@gmail.com>
2024-06-21 21:19:01 +0530

    cart

---
+++ src/components/Products.jsx
@@ -0,0 +1,29 @@
+
+import React, { useContext, useState } from 'react';
+import { productContext } from '../App';
+
+function Products() {
+  const { products , handleDecrement , handleIncrement } = useContext(productContext);
+
+  return (
+    <div className='Products' >
+        <h1 style={{textAlign:"center", padding:"1rem"}}>Products</h1>
+      <ul>
+        {products.map((product, index) => (
+          <div key={index} className="product-item">
+            <li key={product.id}>
+              {product.name} - ${product.price}
+            </li>
+            <div className="quantity-controls">
+              <a onClick={()=> handleIncrement(product.id)}>+</a>
+              <p>{product.quantity}</p>
+              <a onClick={()=> handleDecrement(product.id)}>-</a>
+            </div>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Products;
